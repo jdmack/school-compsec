e6efca888fbd487878f0f556a0d658bdf1e7ab1f
James Mack <wolfbro@gmail.com>
2014-04-08 15:28:37 -0700

    Formatting in sploits

--- sploits/sploit1.c
+++ sploits/sploit1.c
@@ -8,14 +8,15 @@
 
 int main(void)
 {
-  char *args[3];
-  char *env[1];
+    char *args[3];
+    char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
-  env[0] = NULL;
+    args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+    env[0] = NULL;
 
-  if (0 > execve(TARGET, args, env))
-    fprintf(stderr, "execve failed.\n");
+    if(0 > execve(TARGET, args, env)) {
+        fprintf(stderr, "execve failed.\n");
+    }
 
-  return 0;
+    return 0;
 }
