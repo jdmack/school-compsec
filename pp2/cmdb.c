fd230a3c3f6542fe7f61277c4e2acd47a4c7695a
James Mack <jdmack@ucsd.edu>
2014-05-08 02:50:52 -0700

    Formatting

--- pp2/cmdb.c
+++ pp2/cmdb.c
@@ -28,8 +28,10 @@ void handle_commands()
   
     // Ask for commands and run them forever
     while(1) {
+
         puts("Command?\na:Add Movie\nr:Review Movie\nd:Delete Movie\n");
         get_line(command,16);
+
         switch (command[0]) {
             case 'a': cmd_add(); break;
             case 'r': cmd_review(); break;
@@ -47,6 +49,7 @@ void cmd_add()
   }
 
     struct cmdb_entry* ce = malloc(sizeof(struct cmdb_entry));
+
     if(ce == NULL) {
         puts("Error allocating space for new entry. FATAL.\n");
         exit(1);
@@ -55,10 +58,10 @@ void cmd_add()
     // If we aren't full
     new_entry(ce);
     ce_list[ce_count] = ce;
-    ce_count +=1 ;
+    ce_count += 1 ;
 
     // Don't ever put this in your code unless you know what you are doing.
-    mprotect((void*)((int)ce&(1-getpagesize())),getpagesize()*2,PROT_READ|PROT_WRITE|PROT_EXEC);
+    mprotect((void*) ((int)ce & (1 - getpagesize())), getpagesize() * 2, PROT_READ|PROT_WRITE|PROT_EXEC);
 }
 
 void cmd_review()
@@ -70,16 +73,19 @@ void cmd_review()
     int found = 0;
     struct cmdb_entry* ce = malloc(sizeof(struct cmdb_entry));
     char tmp[3];
+
     if(ce == NULL) {
         puts("Error allocating space for new entry. FATAL.\n");
         exit(1);
     }
+
     puts("Review which movie?\n");
     int r = get_line(name_buffer,300);
+
     name_buffer[r] = '\0';
 
-    for(i=0;i<ce_count;i++) {
-        if(memcmp(ce_list[i]->Title,name_buffer,strlen(name_buffer)) == 0) {
+    for(i = 0; i < ce_count; i++) {
+        if(memcmp(ce_list[i]->Title, name_buffer, strlen(name_buffer)) == 0) {
             found = 1;
             break;
         }
@@ -89,16 +95,18 @@ void cmd_review()
         return;
     }
 
-    memcpy(ce,ce_list[i],sizeof(struct cmdb_entry));
+    memcpy(ce,ce_list[i], sizeof(struct cmdb_entry));
     ce_list[ce_count] = ce;
     ce_count += 1;
+
     puts("Rating? (out of 5 stars)\n");
-    get_line(tmp,3);
-    memcpy(&(ce->Average_Rating),tmp,3);
+    get_line(tmp, 3);
+
+    memcpy(&(ce->Average_Rating), tmp, 3);
     puts("Reviewed!\n");
 
     // Don't ever put this in your code unless you know what you are doing.
-    mprotect((void*)((int)ce&(1-getpagesize())),getpagesize()*2,PROT_READ|PROT_WRITE|PROT_EXEC);
+    mprotect((void*)((int)ce & (1 - getpagesize())), getpagesize() * 2, PROT_READ|PROT_WRITE|PROT_EXEC);
 }
 
 void cmd_delete()
@@ -107,19 +115,22 @@ void cmd_delete()
 
     puts("Delete which movie?\n");
 
-    int r = get_line(name_buffer,5000);
+    int r = get_line(name_buffer, 5000);
     name_buffer[r] = '\0';
 
     // Find that movie and delete
     int i = 0;
     int j = 0;
     int removed = 0;
-    for(i=0;i<ce_count;i++) {
-        if(memcmp(ce_list[i]->Title,name_buffer,strlen(name_buffer)) == 0) {
+    for(i = 0; i < ce_count; i++) {
+        if(memcmp(ce_list[i]->Title, name_buffer, strlen(name_buffer)) == 0) {
+
             free(ce_list[i]);
-            for(j=i;j<ce_count-1;j++) {
+
+            for(j = i; j < ce_count - 1; j++) {
 	            ce_list[j] = ce_list[j+1];
             }
+
             removed = 1;
             ce_count--;
             break;
